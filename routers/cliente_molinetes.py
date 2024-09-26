97de790701ad83e3d5e014cdd2d8740fa3805b6e
Low <lologg03@gmail.com>
2024-09-26 13:53:36 -0300

    correccion login cliente_molinetes

--- routers/cliente_molinetes.py
+++ routers/cliente_molinetes.py
@@ -22,7 +22,7 @@ crypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.ClienteMolinetes).filter(models.ClienteMolinetes.nombre_usuario == form_data.username).first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(
             status_code=401,
             detail="Credenciales incorrectas",
